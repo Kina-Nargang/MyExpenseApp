82c6a9112e75d581ceb72b6d9ccbdb136373cde7
Kina <35324430+Kina-Nargang@users.noreply.github.com>
2020-03-10 16:05:34 -0700

    add category

--- BudgetManager/BudgetManager/ExpenseEntry.xaml.cs
+++ BudgetManager/BudgetManager/ExpenseEntry.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BudgetManager.Models;
+using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,18 +14,53 @@ namespace BudgetManager
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ExpenseEntry : ContentPage
     {
+        private List<string> categories;
         public ExpenseEntry()
         {
             InitializeComponent();
+            categories = new List<string>();
+            foreach(string name in Enum.GetNames(typeof(ExpenseCategory)))
+            {
+                categories.Add(name.ToString());
+            }
+
+            CategoryPicker.ItemsSource = categories;
         }
 
-        private void OnSaveButtonClicked(object sender, EventArgs e)
+        private async void OnSaveButtonClicked(object sender, EventArgs e)
         {
+            var expense = (Expense)BindingContext;
+            if (string.IsNullOrEmpty(expense.Filename))
+            {
+                var filename = Path.Combine(App.FolderPath, 
+                    $"{Path.GetRandomFileName()}.expenses.txt");
+
+                File.WriteAllText(filename,expense.Text);
+            }
+            else
+            {
+                File.WriteAllText(expense.Filename, expense.Text);
+            }
+
+            await Navigation.PopAsync();
 
         }
 
-        private void OnCancelButtonClicked(object sender, EventArgs e)
+        private async void OnCancelButtonClicked(object sender, EventArgs e)
         {
+            var expense = (Expense)BindingContext;
+
+            if(expense == null)
+            {
+                return;
+            }
+
+            if (File.Exists(expense.Filename))
+            {
+                File.Delete(expense.Filename);
+            }
+
+            await Navigation.PopAsync();
 
         }
 
