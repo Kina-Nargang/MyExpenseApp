a4f96988ec1e4468489731cd8b49174eca202470
Kina <35324430+Kina-Nargang@users.noreply.github.com>
2020-03-14 15:27:53 -0700

    add changes

--- BudgetManager/BudgetManager/MainPage.xaml.cs
+++ BudgetManager/BudgetManager/MainPage.xaml.cs
@@ -23,9 +23,76 @@ namespace BudgetManager
         {
             InitializeComponent();
 
-            BalanceCalulation();
+            //BalanceCalulation();
         }
 
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // set up expense
+            var files = Directory.EnumerateFiles(App.FolderPath, "*.expenses.csv");
+            double ExpenseTotal = 0.00;
+
+            // first time run, expense file doesn't exist yet.
+            // run foreach loop only when files exist
+            if (files == null || !files.Any())
+            {
+                Total = "0.00";
+            }
+            else
+            {
+                foreach (var filename in files)
+                {
+                    string file = File.ReadAllText(filename);
+                    string[] array = file.Split(',');
+
+                    ExpenseTotal += double.Parse(array[1]);
+                }
+                Total = ExpenseTotal.ToString();
+            }
+
+            ShowTotal.Text = "$" + Total;
+
+            //set up budget
+            string filename_budget = Path.GetFileName("*.MonthlyBudget.txt");
+
+            // first time run, budget file doesn't exist yet. Or file is empty
+            if (!File.Exists(filename_budget) || File.ReadAllText(filename_budget).Length == 0)
+            {
+                Budget = "0.00";
+            }
+            else
+            {
+                Budget = File.ReadAllText(filename_budget);
+            }
+
+            ShowBudget.Text = "$" + Budget;
+
+            //balance
+            double balance = double.Parse(Budget) - double.Parse(Total);
+
+            //format: -$0.00
+            if (balance < 0)
+            {
+                balance = -balance;
+                Balance = balance.ToString();
+                ShowBalance.Text = "-$" + Balance;
+            }
+            else if (balance == 0)
+            {
+                ShowBalance.Text = "$0.00";
+            }
+            else
+            {
+                // format: $0.00
+                Balance = balance.ToString();
+                ShowBalance.Text = "$" + Balance;
+            }
+
+
+        }
+/*
         private void BalanceCalulation() { 
 
             // set up expense
@@ -89,7 +156,7 @@ namespace BudgetManager
             }
 
         }
-
+*/
         private async void Expense_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new ExpenseEntry());
@@ -99,5 +166,11 @@ namespace BudgetManager
         {
             await Navigation.PushAsync(new BudgetEntry());
         }
+        
+        private async void Transaction_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ExpenseList());
+        }
+
     }
 }
